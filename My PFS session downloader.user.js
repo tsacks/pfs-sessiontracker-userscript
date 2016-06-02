2446f9b017695231cf0377714574742b3bee55d1
mogmismo <mogmismo@gmail.com>
2016-06-02 18:43:27 -0400

    More discreet by popular demand.

--- My PFS session downloader.user.js
+++ My PFS session downloader.user.js
@@ -2,10 +2,9 @@
 // @id         My-PFS-session-downloader
 // @name         My PFS session downloader
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  Downloads PFS Sessions as JSON file
 // @author       Michael Tracey
-// @include https://secure.paizo.com/*
 // @include https://secure.paizo.com/people/*/sessions
 // @require http://code.jquery.com/jquery-latest.js
 // @require     http://lightswitch05.github.io/table-to-json/javascripts/jquery.tabletojson.min.js
@@ -47,9 +46,23 @@ function copy(e) {
         if ( $('.tp-front-tab > .bb-content:contains("Sessions")').length ){
             console.log('Starting EXPORT stuff for Sessiontracker');
             document.body.addEventListener('click', copy, true);
-            addGlobalStyle('#sessiontracker_overlay { width: 200px; position: fixed; top: 50px; right: 5px; text-align: center; z-index: 99999;  background-color: #FFEFDB;  border-radius: 15px; border: 2px solid #BA6B6B; margin: 10px 10px 0 10px; padding: 10px; }');
+            addGlobalStyle('#clickme { border-radius: 12px 12px 0px 0px; font-weight: 600; background-color: #6E2D2D; color: #FFF;  position: absolute; left: -50px; bottom: 50px; height: 20px; width: 120px; writing-mode: sideways-lr; -webkit-transform: rotate(-90deg);	-moz-transform: rotate(-90deg); -ms-transform: rotate(-90deg); -o-transform: rotate(-90deg); transform: rotate(-90deg);}');
+            addGlobalStyle('#sessiontracker_overlay { min-height: 100px; width: 200px; position: fixed; top: 50px; right: -212px; text-align: center; z-index: 99999;  background-color: #FFEFDB;  border-radius: 15px; border: 2px solid #BA6B6B; margin: 10px 10px 0 10px; padding: 10px; }');
             addGlobalStyle('#sessiontracker_overlay button { -moz-border-radius: 10px; -webkit-border-radius: 0px; border-radius: 0px; -khtml-border-radius: 0px; background-image: none; display: inline-block; padding: 0 10px; font-size: 15px; font-weight: bold; line-height: 34px; text-decoration: none !important; text-shadow: none !important; color: #FFF;    background-color: #6E2D2D; transition: none; border: 0; } ');
-            $("body").append ('<div id="sessiontracker_overlay"><h3>PFS Scenariotracker Export</h3><br /><button id="export_sessiontracker">Export Games</button><div class="player_results"></div><div style="clear:both"></div></div>');
+            $("body").append ('<div id="sessiontracker_overlay"><div id="clickme">&uarr; Export &uarr;</div><h3>PFS Sessions Export</h3><br /><button id="export_sessiontracker">Export Games</button><div class="player_results"></div><div style="clear:both"></div></div>');
+            var rightVal = -280;
+            $("#clickme").click(function () {
+                if (rightVal === 0){
+                    rightVal = -212;
+                    $(this).parent().animate({right: rightVal + 'px'}, {queue: false, duration: 500});
+                    $(this).html("&uarr; EXPORT &uarr;");
+                }
+                else {
+                    rightVal = 0;
+                    $(this).html("&darr; EXPORT &darr;");
+                    $(this).parent().animate({right: rightVal + 'px'}, {queue: false, duration: 500});
+                }
+            });
             $("#export_sessiontracker").click(function(){
                 var table = $('div.bordered-box.tp-content > .bb-content').find('table');
                 var tablejson = table.tableToJSON({
@@ -72,7 +85,6 @@ function copy(e) {
                     }
                 });
                 $.each(tablejson, function(i, item) {
-                    
                     if (tablejson[i].Scenario.length === 0){
                         console.log("REMOVING EMPTY ROW");
                         delete tablejson[i];
@@ -100,7 +112,6 @@ function copy(e) {
                     tablejson[i].season = '';
                     tablejson[i].senario = '';
                     tablejson[i].FullScenarioNumber ='';
-                    
                     tablejson[i].ScenarioName = tablejson[i].Scenario.replace(/\–/,'-');
                     delete tablejson[i].Scenario;
                     var chunks =  tablejson[i].ScenarioName.split(":");
@@ -130,9 +141,9 @@ function copy(e) {
                 var final = str.replace(/\\/g, "");
                 final = final.substr(1);
                 final = final.substr(0, final.length-1);
-                // console.log(final);
-                $("#sessiontracker_overlay").animate( {width:'500'}, 500 );
-                $("#sessiontracker_overlay > .player_results").html("<textarea id='player_json' class='player_json' style='width:98%; height: 200px; border: 1px solid black;'>"+ final +"</textarea><a href='#' data-copytarget='#player_json'>Copy Data to Clipboard</a>");
+                console.log(final);
+                $("#export_sessiontracker").hide();
+                $("#sessiontracker_overlay > .player_results").html("<textarea id='player_json' class='player_json' style='width:80%; border: 1px solid black;'>"+ final +"</textarea><br /><a href='#' data-copytarget='#player_json'>Copy Data to Clipboard</a>");
             });
 
         }
